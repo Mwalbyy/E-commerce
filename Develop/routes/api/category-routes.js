dc6917feec4c29d1f0381348d637fb1a3417e60d
Marcus Walby <marcuswalby24@gmail.com>
2022-03-25 20:53:37 -0400

    added dev

---
+++ Develop/routes/api/category-routes.js
@@ -0,0 +1,44 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', async (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ model: Product}]
+    })
+    res.status(200).json(categoryData)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+});
+
+router.get('/:id', (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product}]
+    })
+    if(!categoryData) {
+      res.status(404).json({ message: 'no Category found with this id found, check to see if you typed the right one'})
+    }
+    res.status(200).json(categoryData)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+});
+
+
+router.post('/', (req, res) => {
+  // create a new category
+});
+
+router.put('/:id', (req, res) => {
+  // update a category by its `id` value
+});
+
+router.delete('/:id', (req, res) => {
+  // delete a category by its `id` value
+});
+
+module.exports = router;
